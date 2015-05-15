90f2f60912fc0b4e51da6fcf690e975013d73a01
Youngbin Jang <jyb0823@gmail.com>
2015-05-15 22:49:06 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express();
-var http = require('http');
+var httpServer = require('http').Server(app);
+var io = require('socket.io').listen(httpServer);
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-var httpServer = http.createServer(app).listen(8080, function(req,res){
-  console.log('Socket IO server has been started');
+httpServer.listen(8080, function(req,res){
+  console.log('Server started at port 8080');
 });
-// upgrade http server to socket.io server
-var io = require('socket.io').listen(httpServer);
 
 io.on('connection', function (socket) {
   console.log("user connected!");
