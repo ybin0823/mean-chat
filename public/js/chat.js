58d20a9202577fbe37d0180e0986c08d2406bf0e
Youngbin Jang <jyb0823@gmail.com>
2015-05-17 15:56:37 +0900

    chat.js에서 chatApp.cofig return의 whitespace 수정

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,25 +9,24 @@ chatApp.config(['$routeProvider', function ($routeProvider) {
 chatApp.factory('socket', function ($rootScope) {
   var socket = io.connect();
   return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
+  	on: function (eventName, callback) {
+    	socket.on(eventName, function () {  
+    		var args = arguments;
+    		$rootScope.$apply(function () {
+    			callback.apply(socket, args);
+    		});
+    	});
     },
     emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
+    	socket.emit(eventName, data, function () {
+    		var args = arguments;
+    		$rootScope.$apply(function () {
+    			if (callback) {
+    				callback.apply(socket, args);
+    			}
+    		});
+    	});
+    }};
 });
 
 chatApp.controller('chatCtrl', function ($scope, socket) {
