86de10ae90f46194a3d5ad4ce0951177a6ab58da
Youngbin Jang <jyb0823@gmail.com>
2015-05-17 17:38:47 +0900

    방 개설과 방 목록 불러오기 기능 구현

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ var express = require('express');
 var app = express();
 var httpServer = require('http').Server(app);
 var io = require('socket.io').listen(httpServer);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(bodyParser.json());
 
 httpServer.listen(8080, function(req,res){
   console.log('Server started at port 8080');
 });
 
+var users = [];
+var rooms = [];
+
 io.on('connection', function (socket) {
-  var userNameFlag = false;
+  var addedUser = false;
 
   console.log("user connected!");
 
   socket.on('init', function (userName) {
   	console.log("User " + userName + " started chatting!");
   	socket.userName = userName;
+  	users[userName] = userName;
+  	addedUser = true;
   });
 
   socket.on('disconnect', function () {
@@ -29,4 +36,16 @@ io.on('connection', function (socket) {
   	// socket.broadcast.emit('send message', { name : socket.name, message : message });
   	io.emit('send message', { userName : socket.userName, message : message });
   });
+});
+
+app.get('/rooms', function (req, res) {
+	console.log('GET method from request!')
+	res.json(rooms);
+});
+
+app.post('/rooms', function (req, res) {
+	console.log(req.body);
+	rooms.push({ roomName : req.body.roomName, creator: req.body.userName })
+	console.log(rooms);
+	res.json(rooms);
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,7 @@ var chatApp = angular.module('chatApp', ['ngRoute']);
 chatApp.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 	.when('/', { templateUrl : 'partials/name.html' })
+	.when('/rooms', { templateUrl : 'partials/rooms.html' })
 	.when('/chat', { templateUrl : 'partials/chat.html' })
 	.otherwise({ redirectTo: '/name.html' });
 }]);
@@ -26,8 +27,8 @@ chatApp.factory('socket', function ($rootScope) {
     			}
     		});
     	});
-    }};
-});
+    }
+}});
 
 chatApp.controller('chatCtrl', function ($scope, socket) {
 	$scope.chatList = [];
@@ -44,10 +45,27 @@ chatApp.controller('chatCtrl', function ($scope, socket) {
 	});
 });
 
-chatApp.controller('nameCtrl', function ($scope, $location, socket) {
+chatApp.controller('nameCtrl', function ($scope, $rootScope, $location, socket) {
 	$scope.startChat = function () {
 		console.log($scope.userName);
+		$rootScope.userName = $scope.userName;
 		socket.emit('init', $scope.userName);
-		$location.path('/chat');
+		$location.path('/rooms');
+	}
+});
+	
+chatApp.controller('roomCtrl', function ($scope, $http) {
+	$http.get('/rooms').success(function (res) {
+		console.log('Received rooms from server')
+		$scope.rooms = res;
+	})
+
+	$scope.createRoom = function () {
+		console.log($scope.roomName);
+		$http.post('/rooms', { roomName : $scope.roomName, userName : $scope.userName }).success(function (res) {
+			console.log(res);
+			$scope.rooms = res;
+		});
+		$scope.roomName = '';
 	}
 });
